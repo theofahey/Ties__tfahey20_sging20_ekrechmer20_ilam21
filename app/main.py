fb5272d4a667514aefefb86f084e33b8df1d8787
Theofahey <tfahey20@stuy.edu>
2021-12-13 11:41:20 -0500

    Basic FBI API implementation

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from flask import render_template, redirect, request, url_for, session, Flask
 import urllib.request
 from urllib.request import urlopen
 import json
+import random
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -13,9 +14,16 @@ def start():
 @app.route("/FBI", methods = ['GET', 'POST'])
 def kanye_east():
     url = "https://api.kanye.rest"
+    #Opens From Browser in order to ensure not being blocked.
     w = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'})
     r = urllib.request.urlopen(w).read()
     x = json.loads(r)
+    url2 = "https://api.fbi.gov/wanted/v1/list"
+    w2 = urllib.request.Request(url2,headers={'User-Agent': 'Mozilla/5.0'})
+    r2 = urllib.request.urlopen(w2).read()
+    data = json.loads(r2)
+    length = data['total']
+    print(data['items'][random.randrange(0,20)]['title'])
     return render_template("fbi.html", kanye = x["quote"])
 
 if __name__ == "__main__": #false if this file imported as module
