4fe254acbebb147a5e4176befe52483788c4ecc1
sging04 <67232054+sging04@users.noreply.github.com>
2021-12-23 08:29:53 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,8 +7,6 @@ import json
 import random
 import os
 from jinja2 import Template
-from database import Usernamepassword, madlibTable
-
 
 app = Flask(__name__)
 
@@ -30,21 +28,28 @@ def start():
 @app.route("/FBI-input", methods = ['GET', 'POST'])
 def kanye_east_fillin():
     return render_template("FBIfill.html")
-@app.route("/signup")
-def signup():
+@app.route("/login")
+def login():
     username= request.args['username']
     password= request.args['password']
-    passauth= request.args['passauth']
+
     if (username=="" or password==""):
-        return render_template('signup.html', syntaxerror="Cannot submit blank username or password")
+        return render_template('login.html', syntaxerror="Cannot submit blank username or password")
     elif not userpass.userExists(username):
-        userpass.insert(username, password) # committing actions to database must be done every time you commit a command
-        session["username"]=username
-        return redirect("/loggedin")
-    elif (password!=passauth):
-        return render_template('signup.html', passerror="Passwords must match")
+        return render_template('login.html', syntaxerror="Username does not exist")
+    elif not userpass.passMatch(username, password):
+        return render_template('login.html', syntaxerror = "Incorrect password")
     else:
-        return render_template('signup.html', syntaxerror = "This username already exists")
+        session["username"] = username
+        return redirect('/loggedin')
+
+@app.route("/signupdisplay")
+def disp_signuppage():
+    if (session.get("username") is not None):
+        # if there's an existing session, shows welcome page
+        return redirect ("/")
+    if ("username" != None):
+        return render_template( 'signup.html' )
 
 @app.route("/signup")
 def signup():
@@ -61,6 +66,10 @@ def signup():
         return render_template('signup.html', passerror="Passwords must match")
     else:
         return render_template('signup.html', syntaxerror = "This username already exists")
+
+@app.route("/loggedin")
+def loggedin(): # does not show info in URL, shows /loggedin instead
+    return redirect("/")
 
 
 @app.route("/FBI", methods = ['GET', 'POST'])
